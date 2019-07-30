80df8716d06ebfc8e271b94e6b5bc992908de14b
Luke Harris <lukeharris01170@gmail.com>
2019-07-30 21:39:41 +0100

    Add getting started info to docs page

--- docs/index.tsx
+++ docs/index.tsx
@@ -33,22 +33,51 @@ const inputExampleSource = cleanSrc(
   readFileSync(__dirname + "/input-example.tsx", "utf-8").toString()
 );
 
+const guideSource = `import React from "react";
+import ReactDOM from "react-dom";
+
+import { Button, lightTheme } from "imprest";
+import { ThemeContext } from "@emotion/core";
+
+ReactDOM.render(
+  <ThemeContext.Provider value={lightTheme}>
+    <Button>Hello world!</Button>
+  </ThemeContext.Provider>,
+  document.getElementById("root")
+);`;
+
 render(
   <ThemeContext.Provider value={lightTheme}>
     <Global styles={globalStyles(lightTheme)} />
     <PageTemplate>
       <section>
-        <h1>Button</h1>
+        <h1>Imprest</h1>
+      </section>
+      <section>
+        <h2>Getting started</h2>
+        <p>To install Imprest run the following command:</p>
+        <CodeBlock
+          language="bash"
+          code={`yarn add imprest react react-dom @emotion/core @emotion/styled`}
+        />
+        <p>
+          To use Imprest's components, you must wrap your application with a
+          theme context provider and specifying any valid imprest theme:
+        </p>
+        <CodeBlock showLineNumbers language="jsx" code={guideSource} />
+      </section>
+      <section>
+        <h2>Button</h2>
         <ButtonExample />
         <CodeBlock showLineNumbers language="jsx" code={buttonExampleSource} />
       </section>
       <section>
-        <h1>Card</h1>
+        <h2>Card</h2>
         <CardExample />
         <CodeBlock showLineNumbers language="jsx" code={cardExampleSource} />
       </section>
       <section>
-        <h1>Input</h1>
+        <h2>Input</h2>
         <InputExample />
         <CodeBlock showLineNumbers language="jsx" code={inputExampleSource} />
       </section>
