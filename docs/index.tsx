24f073f9e63d6f6b50a09b61977f9380e898bfa8
Luke Harris <lukeharris01170@gmail.com>
2019-07-30 21:52:27 +0100

    Add CodeBlock example

--- docs/index.tsx
+++ docs/index.tsx
@@ -3,20 +3,12 @@ import { render } from "react-dom";
 import { readFileSync } from "fs";
 
 import { ThemeContext, Global } from "@emotion/core";
-import {
-  Button,
-  Card,
-  Input,
-  PageTemplate,
-  lightTheme,
-  Spacing,
-  globalStyles,
-  CodeBlock
-} from "../src";
+import { PageTemplate, lightTheme, globalStyles, CodeBlock } from "../src";
 
 import ButtonExample from "./button-example";
 import CardExample from "./card-example";
 import InputExample from "./input-example";
+import CodeBlockExample from "./code-block-example";
 
 const cleanSrc = (src: string) =>
   src.replace(/\.\.\/src/, "imprest").replace(/\n$/, "");
@@ -31,6 +23,10 @@ const cardExampleSource = cleanSrc(
 
 const inputExampleSource = cleanSrc(
   readFileSync(__dirname + "/input-example.tsx", "utf-8").toString()
+);
+
+const codeBlockExampleSource = cleanSrc(
+  readFileSync(__dirname + "/code-block-example.tsx", "utf-8").toString()
 );
 
 const guideSource = `import React from "react";
@@ -80,6 +76,15 @@ render(
         <h2>Input</h2>
         <InputExample />
         <CodeBlock showLineNumbers language="jsx" code={inputExampleSource} />
+      </section>
+      <section>
+        <h2>CodeBlock</h2>
+        <CodeBlockExample />
+        <CodeBlock
+          showLineNumbers
+          language="jsx"
+          code={codeBlockExampleSource}
+        />
       </section>
     </PageTemplate>
   </ThemeContext.Provider>,
